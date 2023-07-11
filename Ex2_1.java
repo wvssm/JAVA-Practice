ce6f9d1f0b92812c189986eeb8ec0fde480b453a
wvssm <52875244+wvssm@users.noreply.github.com>
2023-07-11 23:51:22 +0900

    2023_07_11

---
+++ Ex2_1.java
@@ -0,0 +1,12 @@
+
+public class Ex2_1 {
+	public static void main(String[] args) {
+		//sop ctrl + Space 자동완성
+		//ctrl + alt + down 복사하며 줄바꿈
+		//alt + shift + a 칼럼 편집 모드
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3.0);
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl + shift + L 단축키 전체 목록
+		// ctrl + +,- 폰트크기 증가/ 감소
+		// alt + shift + A 멀티 컬럼 편집
+		// ctrl + i 자동 들여쓰기
+		// ctrl + / 주석(토글)
+		
+		System.out.println("Hello World");
+	}
+}
