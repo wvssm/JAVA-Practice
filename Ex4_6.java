e8140578d6acc39b48d427dfb2bffb567471ada3
wvssm <52875244+wvssm@users.noreply.github.com>
2023-07-13 03:04:58 +0900

    2023-07-13

---
+++ Ex4_6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex4_6 {
+	public static void main(String args[]) {
+		System.out.print("현재의 월을 입력하세요.>");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		
+		switch (month) {
+		case 3: case 4: case 5:
+			System.out.println("현재의 계절은 봄 입니다.");
+			break;
+		case 6: case 7: case 8:
+			System.out.println("현재의 계절은 여름 입니다.");
+			break;
+		case 9: case 10: case 11:
+			System.out.println("현재의 계절은 가을 입니다.");
+			break;
+		default:
+			System.out.println("현재의 계절은 겨울 입니다.");
+//			break; // default에는 break 없어도 됨
+		}
+	}
+}
---
+++ Ex4_7.java
@@ -0,0 +1,14 @@
+
+public class Ex4_7 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		for(int i=0; i<20; i++) {
+			num = (int)(Math.random() * 11) -5;
+			System.out.println(num);
+		}
+
+	}
+
+}
